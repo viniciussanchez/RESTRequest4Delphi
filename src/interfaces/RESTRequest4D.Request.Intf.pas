a5614db505a759434636ac8c6889630979bc3d6b
Danilo Lucas <developer.dlio@gmail.com>
2020-07-22 12:33:44 -0300

    UserAgent function implemented

--- src/interfaces/RESTRequest4D.Request.Intf.pas
+++ src/interfaces/RESTRequest4D.Request.Intf.pas
@@ -41,6 +41,7 @@ type
     function ClearHeaders: IRequest;
     function AddHeader(const AName, AValue: string; const AOptions: TRESTRequestParameterOptions = []): IRequest;
     function ClearParams: IRequest;
+	function UserAgent(const AName: string): IRequest;
     {$IF COMPILERVERSION < 33}
       function AddParam(const AName, AValue: string; const AKind: TRESTRequestParameterKind = TRESTRequestParameterKind.pkGETorPOST): IRequest;
     {$ELSE}
